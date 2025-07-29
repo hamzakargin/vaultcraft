65a4c08f032782243a8fbabbb00ce03b94da555b
hamzakargin <hamzakargin@outlook.com>
2025-07-29 15:27:01 +0300

    Login method & page done

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -2,3 +2,40 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    console.log(reqBody);
+    //check if user exist
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json({ error: "User doesnt exist" }, { status: 400 });
+    }
+    //check if password valid
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json({ error: "Invalid password" }, { status: 400 });
+    }
+    //create token data
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+    //cretae token
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+    const response = NextResponse.json({
+      message: "Login successfully",
+      success: true,
+    });
+    response.cookies.set("token", token, { httpOnly: true });
+    return response;
+  } catch (error) {}
+}
